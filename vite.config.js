8bc77f1d6a5d088d890099753ef2f9eb8b1b100a
Patricia <p.jannenc@gmail.com>
2026-04-13 12:43:37 -0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/patricia0janne.github.io/Climax/",
+  base: "/Climax/",
 });
